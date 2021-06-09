6ade491dbc003e51081f982b7d38b778d7e24120
Amy Joseph <amy.beth.joseph@gmail.com>
2021-06-09 21:01:47 +1200

    Update email_renamer.py
    Minimal suggested changes. More verbose comments and some more verbose variable names for clarity.

--- scripts/email_renamer.py
+++ scripts/email_renamer.py
@@ -4,16 +4,16 @@ import glob
 import extract_msg
 import hashlib
 
-"""Prepends the rvc datetime of an msg email file as a string to the .msg file name
+"""Prepends the received datetime of an msg email file as a string to the .msg file name
 Delimited by a #
-yyyy_mm_dd-hh_mm_ss 
-
-creates log file that lists old f_name, new f_name, and file fixity. (| delimited)
+i.e. 'yyyy_mm_dd-hh_mm_ss#[original_filename].msg'
+Acts on all .msg files in a given folder
+Creates log file that lists original filepath, new filename, and file fixity. (| delimited)
 """
 ### set this to the name of your log file.
 my_log_file = "log.txt"
 
-### set to folder to process.
+### set to filepath of folder containing .msg files to process.
 folder = r"folder"
 
 
@@ -27,25 +27,25 @@ def md5(fname):
 
 log_data = []
 for f in [x for x in os.listdir(folder) if x.endswith(".msg")]:
-	my_f = os.path.join(folder, f)
+	msg_filepath = os.path.join(folder, f)
 
-	my_glob = glob.glob(my_f)
+	msg_glob = glob.glob(msg_filepath)
 
-	for filename in my_glob:
+	for filename in msg_glob:
 		msg = extract_msg.Message(filename)
 		msg_sender = msg.sender
 		msg_date = msg.date
-		msg_subj = msg.subject
-		msg_message = msg.body
-		my_date = parse(msg_date)
-		my_date_string = my_date.strftime("%Y_%m_%d-%H_%M_%S")
-		new_f_name = my_date_string+"#"+f
-		new_f_path = os.path.join(folder, new_f_name)
+		msg_subject = msg.subject
+		msg_body = msg.body
+		msg_datetime = parse(msg_date)
+		msg_date_string = msg_datetime.strftime("%Y_%m_%d-%H_%M_%S")
+		new_filename = msg_date_string+"#"+f
+		new_filepath = os.path.join(folder, new_filename)
 		msg.close()
 
-	my_md5 = md5(my_f)
-	os.rename(my_f,new_f_path)
-	log_data.append(f"{my_f}|{new_f_name}|{my_md5}")
+	msg_file_md5 = md5(msg_file)
+	os.rename(msg_filepath,new_filepath)
+	log_data.append(f"{msg_filepath}|{new_filename}|{msg_file_md5}")
 
 with open(my_log_file, "w", encoding = "utf8") as data:
 	data.write("\n".join(log_data))
