1a45686286715cb16c1ada204fa725fc9a7a0684
Amy Joseph <amy.beth.joseph@gmail.com>
2021-06-14 17:30:47 +1200

    Update email_renamer_reverter.py
    new_filename=f.split("#",1)[1] covers any case where '#' is elsewhere in the filename - will only split on the first occurence.

--- scripts/email_renamer_reverter.py
+++ scripts/email_renamer_reverter.py
@@ -24,7 +24,7 @@ def md5(fname):
 log_data =[]
 for f in [x for x in os.listdir(folder) if x.endswith(".msg")]:
     msg_filepath = os.path.join(folder, f)
-    new_filename=f.split("#")[1]
+    new_filename=f.split("#",1)[1]
     new_filepath = os.path.join(folder, new_filename)
     msg_file_md5 = md5(msg_filepath)
     #If the 'cleaned' filename already exists in the folder, the filename will not be changed back
